f7d0b6ec930613181a4a5223c046a6ff4c6de12e
aartoo <contacttothis@gmail.com>
2017-08-28 20:43:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,3 +14,16 @@ button.onclick = function () {
     request.open('GET','http://contacttothis21.imad.hasura-app.io/counter',true); 
     request.send(null);
 };
+var inputName = document.getElementById('name');
+var name = inputName.value;
+var submit = document.getElementById('submit_button');
+submit.onClick = function () {
+ 
+ var names = ['name1','name2','name3','name4'];
+ var list = ' ';
+ for (var i=0 ; i<names.length; i++) {
+     list += '<li>' + names[i] + '</li>';
+ }
+var ul = getElemetById('namelist');
+ul.innerHTML  = list;
+};
