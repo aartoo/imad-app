2058fb9e7b3e6b710fa70ab1dad827f56c7884e1
aartoo <contacttothis@gmail.com>
2017-08-28 21:04:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,6 +23,6 @@ submit.onClick = function () {
  for (var i=0 ; i<names.length; i++) {
      list += '<li>' + names[i] + '</li>';
  }
-var ul = getElemetById('namelist');
+var ul = getElementById('namelist');
 ul.innerHTML = list;
 };
